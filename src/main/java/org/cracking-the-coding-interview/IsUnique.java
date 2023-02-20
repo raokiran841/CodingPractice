5ed27ac0afbe84b7e7201ebbfb2a21565ddea207
kiran rao <rao.kiran841@gmail.com>
2023-02-20 21:02:57 +0530

    adding practice codes to github

---
+++ src/main/java/org/cracking-the-coding-interview/IsUnique.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IsUnique {
+    /*
+    Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures?
+    * */
+    public static void main(String[] args) {
+        boolean res = isUnique("abcdef");
+        System.out.println(res);
+    }
+
+    private static boolean isUnique(String str){
+        //with additional Data Structure (HashMap)
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(Character ch: str.toCharArray()){
+            if(hm.get(ch) != null){
+                return false;
+            }else {
+                hm.put(ch,1);
+            }
+        }
+        return true;
+    }
+}
